239956ef135f20a16a85058ff5583e4a5a3cb913
stone <iamnotstone@sina.com>
2018-05-16 13:12:08 +0800

    add delay

--- src/reactComponent.js
+++ src/reactComponent.js
@@ -84,8 +84,8 @@ class ReactComponent extends React.Component{
   componentDidMount(){
     width = container.offsetWidth
     height = container.offsetHeight
-    setTimeout(this.initThree.bind(this), 1000)
-	  //this.initThree()
+    //setTimeout(this.initThree.bind(this), 1000)
+	  this.initThree()
     container.addEventListener('touchstart', this._onDocumentTouchStart, false);
     container.addEventListener('touchmove', this._onDocumentTouchMove, false);  
 		window.addEventListener('resize', this._onDocumentResize, false)
